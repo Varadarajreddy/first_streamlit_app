594489d253795e90790ad03dea3f0aaa531f26f8
Varadaraj <vr20aaj@outlook.com>
2022-08-05 16:10:36 +0100

    Update streamlit_app.py
    comma

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My parents new healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text(print(r"\U+1F414")'Omega 3 & Blue berry Meal')
+streamlit.text(print(r"\U+1F414"),'Omega 3 & Blue berry Meal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-boiled free-range Egg')
 streamlit.text('Avacoda toast')
